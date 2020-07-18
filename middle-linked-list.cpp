5e00bdae96bb71904fd99c1811649715e755f34d
Andrew Shitov <andy@shitov.ru>
2020-07-18 13:06:38 +0200

    Terminology and destroy

--- middle-linked-list.cpp
+++ middle-linked-list.cpp
@@ -6,51 +6,61 @@
 
 using namespace std;
 
-struct Node {
-    int value;
-    Node* next;
+struct node {
+    int data;
+    node* next;
 };
 
-Node* create_list() {
+node* create_list() {
     srand(time(NULL));
     const int nodes = 10 + rand() % 10;
 
-    Node* first = nullptr;
-    Node* prev = nullptr;
+    node* head = nullptr;
+    node* prev = nullptr;
 
     for (int n = 0; n < nodes; n++) {
-        Node* curr = new Node();
-        curr->value = rand() % 100;
+        node* curr = new node();
+        curr->data = rand() % 100;
         if (prev) prev->next = curr;
 
-        if (!first) first = curr;
+        if (!head) head = curr;
         prev = curr;
     }
     prev->next = NULL;
 
-    return first;
+    return head;
 }
 
-void display_list(Node* first) {
-    while (first) {
-        cout << first->value;
-        if (first->next) cout << ", ";
-        first = first->next;
+void display_list(node* head) {
+    while (head) {
+        cout << head->data;
+        if (head->next) cout << ", ";
+        head = head->next;
     }
     cout << "." << endl;
 }
 
-int main() {
-    Node* first = create_list();
-    display_list(first);
+void destroy_list(node* head) {
+    while (head) {
+        node* next = head->next;
+        delete head;
+        head = next;
+    }
+}
 
-    Node* middle = first;
+int main() {
+    node* head = create_list();
+    display_list(head);
+
+    node* middle = head;
     bool move = false;
-    while (first) {
+    while (head) {
         if (move) middle = middle->next;
         move = !move;
-        first = first->next;
+        head = head->next;
     }
 
-    cout << middle->value << endl;
+    cout << middle->data << endl;
+
+    destroy_list(head);
 }
